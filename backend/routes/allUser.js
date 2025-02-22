ca1c305cf05ec48086a0ad229086805fa7932fd0
Ganesh-Mk <ganeshmk247@gmail.com>
2025-02-23 03:24:19 +0530

    demo

--- backend/routes/allUser.js
+++ backend/routes/allUser.js
@@ -6,8 +6,8 @@ router.get("/allUser", async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error", error: error.message });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server err", err: err.message });
   }
 });
 
